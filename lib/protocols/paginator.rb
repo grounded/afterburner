1da6828db2861f428796763cffec7dd4656a960b
Rob Yurkowski <rob@yurkowski.net>
2012-09-16 22:43:08 +0000

    Add some documentation to the pagination wrapper.

--- lib/protocols/paginator.rb
+++ lib/protocols/paginator.rb
@@ -1,4 +1,16 @@
 module Afterburner
+  # Wraps pagination so that users can swap out pagination models with the use
+  # of any one pagination plugin. At present, it only supports a single 
+  # pagination method, but it will pass along any arguments given to that 
+  # method.
+  #
+  # Simply set Afterburner::Pagination.paginating_method equal to the string
+  # name of your paginating method, and then use the ::paged method on queries.
+  # For example, if you are wrapping will_paginate, the pagination method there
+  # is called 'paginate'. Thus:
+  #
+  #     Afterburner::Paginator.paginating_method = 'paginate'
+  #     MyModel.where('field' => true).paged(:per_page => 10, :blah => false)
   module Paginator
     def self.paginating_method
       @@paginating_method
