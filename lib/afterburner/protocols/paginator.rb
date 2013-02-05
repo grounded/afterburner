408e5e7fb59a0990b5039887f17b24695eea68c4
Josh Adams <josh@isotope11.com>
2013-02-04 19:48:08 -0600

    Prefer specifying protocol method with symbols
    It just seems more appropriate.  It's the right color for this shed.

--- lib/afterburner/protocols/paginator.rb
+++ lib/afterburner/protocols/paginator.rb
@@ -5,18 +5,18 @@ module Afterburner
     # pagination method, but it will pass along any arguments given to that
     # method.
     #
-    # Simply set Afterburner::Pagination.paginating_method equal to the string
+    # Simply set Afterburner::Pagination.paginating_method equal to the symbol
     # name of your paginating method, and then use the as_pages method on queries.
     # For example, if you are wrapping will_paginate, the pagination method there
     # is called 'paginate'. Thus:
     #
-    #     Afterburner::Paginator.paginating_method = 'paginate'
+    #     Afterburner::Paginator.paginating_method = :paginate
     #     MyModel.where('field' => true).as_pages(:per_page => 10, :blah => false)
     #
     # You can also set paginating_method on the model itself to use a different
     # paginator for just that model. For example:
     #
-    #     MyModel.paginating_method = 'page'
+    #     MyModel.paginating_method = :page
     #     MyModel.where(:field => true).as_pages("cats") # uses 'local' page method
     #
     # This won't interrupt other models' ability to use the default paginator.
--- lib/afterburner/protocols/searcher.rb
+++ lib/afterburner/protocols/searcher.rb
@@ -5,18 +5,18 @@ module Afterburner
     # searching method, but it will pass along any arguments given to that
     # method.
     #
-    # Simply set Afterburner::Searcher.searching_method equal to the string or
-    # symbol name of your searching method, and then use the :as_search_results
-    # method for queries. For example, if you are wrapping acts_as_indexed, the
-    # searching method there is called 'with_query'. Thus:
+    # Simply set Afterburner::Searcher.searching_method equal to the symbol name
+    # of your searching method, and then use the :as_search_results method for 
+    # queries. For example, if you are wrapping acts_as_indexed, the searching 
+    # method there is called 'with_query'. Thus:
     #
-    #     Afterburner::Searcher.searching_method = 'with_query'
+    #     Afterburner::Searcher.searching_method = :with_query
     #     MyModel.where(:field => true).as_search_results("cats") # uses with_query
     #
     # You can also set searching_method on the model itself to use a different
     # as_search_results for just that model. For example:
     #
-    #     MyModel.searching_method = 'local_query'
+    #     MyModel.searching_method = :local_query
     #     MyModel.where(:field => true).as_search_results("cats") # uses local_query
     #
     # This won't interrupt other models' ability to use the default as_search_results.
