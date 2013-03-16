d91f150581b23d06603d137cbf42e538785ac4db
Philip Arndt <parndt@gmail.com>
2013-03-16 18:45:43 +1300

    Specify the actual aliases for adapters/paginator

--- spec/shared/afterburner/adapters/paginator.rb
+++ spec/shared/afterburner/adapters/paginator.rb
@@ -1,8 +1,8 @@
 shared_examples_for "adapters/paginator" do
 
   let(:dummy_class) { nil }
-  let(:with_per_page_alias) { nil }
-  let(:with_total_pages) { nil }
+  let(:per_page_alias) { nil }
+  let(:total_pages_alias) { nil }
 
   it 'redirects with_items_per_page properly' do
     dummy_class.should_receive(per_page_alias).once.with(1).and_return true
