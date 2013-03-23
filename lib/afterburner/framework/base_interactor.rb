e626270a2ea9fa344424bacf0f7e2f7be0462e61
Philip Arndt <parndt@gmail.com>
2013-03-24 12:21:43 +1300

    Made the BaseInteractor behave like the BaseConductor

--- lib/afterburner/framework/base_interactor.rb
+++ lib/afterburner/framework/base_interactor.rb
@@ -1,10 +1,14 @@
 module Afterburner
   module Framework
     class BaseInteractor
-      class << self
-        def call(data, options = {})
-          new(data, options).to_response
-        end
+      # Runs the interactor and returns a hash of objects (presenter or
+      # otherwise) that are passed to the view.
+      #
+      # @param data [Hash] Data supplied to the interactor.
+      # @param options [Hash] A series of options that override default values.
+      # @return [Hash] An associative array of presenter objects to be passed to the view.
+      def self.call(data, options = {})
+        new(data, options).to_response
       end
 
       def to_response
@@ -12,7 +16,20 @@ module Afterburner
       end
 
       protected
+      attr_accessor :data, :options
       def initialize(data, options = {})
+        self.data = data
+        self.options = defaults.merge(options)
+      end
+
+      # Provides a frozen hash of sane defaults that can be overridden to change class
+      # behaviour. Highly useful for dependency injection to simplify testing. For
+      # instance, instead of hard-coding a repository object, specify a default for it
+      # so that you can pass in a mock when testing.
+      #
+      # @return [Hash] An associative array of defaults.
+      def defaults
+        Hash.new.freeze
       end
     end
   end
