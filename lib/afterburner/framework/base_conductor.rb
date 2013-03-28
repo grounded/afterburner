f6238bd5ebb95e62d0ed70a03a98523adc39330e
Rob Yurkowski <rob@yurkowski.net>
2013-03-28 02:13:59 -0400

    Pull out a few more common idioms in base_conductor, and clarify that it is not responsible for wrapping its return data in presenters.

--- lib/afterburner/framework/base_conductor.rb
+++ lib/afterburner/framework/base_conductor.rb
@@ -1,17 +1,20 @@
+require 'ostruct'
+
 module Afterburner
   module Framework
     # The Base Conductor is the conductor that all other conductors inherit
     # from. Its primary role is to decipher the parameters, retrieve or save
     # data to or from data-facing APIs like a database or a third-party API,
     # and to optionally wrap what it plans to return in an object that provides
-    # methods for access (in some cases, abstracting complex logic away from
-    # the view).
+    # methods for access.
     #
     # The Conductor is not responsible, however, for validating or wrapping the
-    # data it retrieves. That task is delegated down to the application. Nor,
+    # data it retrieves. That task is delegated down to the entities. Nor,
     # however, is it responsible for selecting which data-facing APIs it uses;
     # instead, it has its dependencies injected by its calling controller. The
-    # conductor should simply `call` them as appropriate.
+    # conductor should simply `call` them as appropriate. Finally, it is not
+    # responsible for presenting its entities in any which way -- this it
+    # leaves to the controller.
     class BaseConductor
       # Create a new conductor.
       #
@@ -20,23 +23,25 @@ module Afterburner
       #   defaults, used to configure the object.
       # @return [Conductor] An instance of the conductor.
       def initialize(params, options = {})
-        self.params = params
-        self.options = defaults.merge(options)
+        self.defaults = Hash.new.freeze
+        self.params   = params
+        self.options  = defaults.merge(options)
+
+        self.repositories = OpenStruct.new(options[:repositories])
       end
 
-      # Transforms the data returned by the #data method into an acceptable
-      # format to return to the controller. Should begin the conductor's work
+      # Returns data to the controller. Should begin the conductor's work
       # beyond setting options.
       #
       # @return [Hash] An associative array of data to be passed to the view
-      #   (usually presenters or entities).
+      #   (usually entities).
       def call
         raise NotImplementedError,
           "#call must be overridden when subclassing BaseConductor"
       end
 
       protected
-      attr_accessor :params, :options
+      attr_accessor :defaults, :params, :options, :repositories
 
       # Passes data retrieved from somewhere down to Interactors and returns
       # something to be wrapped by #to_response -- by convention, an
@@ -49,16 +54,6 @@ module Afterburner
       def data
         raise NotImplementedError,
           "#data must be overridden when subclassing BaseConductor."
-      end
-
-      # Provides a frozen hash of sane defaults that can be overridden to change
-      # class behaviour. Highly useful for dependency injection to simplify
-      # testing. For instance, instead of hard-coding a repository object,
-      # specify a default for it so that you can pass in a mock when testing.
-      #
-      # @return [Hash] An associative array of defaults.
-      def defaults
-        Hash.new.freeze
       end
     end
   end
--- spec/afterburner/framework/base_conductor_spec.rb
+++ spec/afterburner/framework/base_conductor_spec.rb
@@ -1,16 +1,46 @@
-require 'shared/callable'
 require 'afterburner/framework/base_conductor'
 
 module Afterburner
   module Framework
     describe BaseConductor do
       subject { BaseConductor }
-      it_behaves_like "a callable"
 
       describe "instantiation" do
+        it "merges options onto defaults" do
+          instance = subject.new("blah", {:pages => "value"})
+          instance.send(:options).keys.should include(:pages)
+        end
+
+        it "overwrites defaults with options" do
+          subject.any_instance.stub(:defaults).and_return({:pages => "value"})
+          instance = subject.new("blah", {:pages => "new value"})
+          instance.send(:options)[:pages].should == "new value"
+        end
+
         it "stores parameters" do
           instance = subject.new("blah", {})
           instance.send(:params).should == "blah"
+        end
+
+        it "prettifies repositories" do
+          repository = double "repository"
+          instance = subject.new("blah", {:repositories => {:page => repository}})
+          instance.send(:repositories).should respond_to(:page)
+        end
+
+      end
+
+      describe "invocation" do
+        it "raises an error if not implemented" do
+          instance = subject.new({}, {})
+          expect(lambda { instance.call }).to raise_error NotImplementedError
+        end
+      end
+
+      describe "defaults" do
+        it "should be empty if not implemented" do
+          instance = subject.new({}, {})
+          instance.send(:defaults).should be_empty
         end
       end
 
