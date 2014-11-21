963c0bc2831695157b9b0b997806fcb75de2e58f
Philip Arndt <git@arndt.io>
2014-11-21 14:15:19 -0800

    More honesty with the email address.

--- lib/afterburner/package.rb
+++ lib/afterburner/package.rb
@@ -1,7 +1,7 @@
 module Afterburner
   VERSION       = '0.0.3'
   AUTHORS       = ["The Afterburner Team"]
-  EMAILS        = ["info@afterburnercms.com"]
+  EMAILS        = ["not-yet@example.org"]
   HOMEPAGE      = "https://github.com/grounded/"
   SUMMARY       = "A framework for delivery-independent applications and hot-swappability."
   DESCRIPTION   = SUMMARY
